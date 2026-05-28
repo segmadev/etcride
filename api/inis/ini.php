f57d758912696f01b32e19f82ac82b1940db6072
segma.dev <serikioluwagbenga@gmail.com>
2026-05-28 16:44:47 +0100

    allow more methods

--- api/inis/ini.php
+++ api/inis/ini.php
@@ -1,6 +1,13 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Authorization, Content-Type, Accept, X-Requested-With");
+
+if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
 $parsedUrl = parse_url($_SERVER['REQUEST_URI']);
 $cleanPath = rtrim($parsedUrl['path'] ?? '/', '/');
 define("ISAPI", true);
